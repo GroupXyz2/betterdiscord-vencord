00e03310f9d2cd636474d59a6ed291c3935f4375
GroupXyz <122353547+GroupXyz2@users.noreply.github.com>
2024-12-03 16:31:49 +0100

    Add files via upload

---
+++ consoleKey.plugin.js
@@ -0,0 +1,28 @@
+/**
+ * @name OpenConsole
+ * @author GroupXyz
+ * @version 1.0.0
+ * @description Öffnet die Entwicklerkonsole mit Strg+Umschalt+I.
+ * @website https://github.com/GroupXyz2
+ * @source https://github.com/GroupXyz2/betterdiscord/consoleKey.plugin.js
+ * @updateUrl https://github.com/GroupXyz2/betterdiscord/consoleKey.plugin.js
+ */
+
+module.exports = class OpenConsole {
+    start() {
+        document.addEventListener("keydown", this.openConsole);
+    }
+
+    stop() {
+        document.removeEventListener("keydown", this.openConsole);
+    }
+
+    openConsole(event) {
+        // Check if the pressed keys are Ctrl + Shift + I
+        if (event.ctrlKey && event.shiftKey && event.key === "I") {
+            // Open the developer tools
+            require("electron").remote.getCurrentWindow().toggleDevTools();
+        }
+    }
+};
+
